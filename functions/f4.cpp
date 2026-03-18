b0905f15dd38d9e405058750f2a90ca8e96a2a33
Ashasahu01 <as20252651013@gmail.com>
2026-03-18 22:58:16 +0530

    modify my code

--- functions/f4.cpp
+++ functions/f4.cpp
@@ -6,17 +6,18 @@ int mult(int a, int b){
     return mult;
 }
 
+//check for even odd
 int evenOdd(int num){
     if(num%2 == 0){
-        cout << "Even\n" ;
+        cout << "Even\n" ; //print even
     }else {
-        cout << "Odd\n";
+        cout << "Odd\n"; //print odd
     }
     return 0;
 }
 
 int main(){
-    cout << mult(2,2) << endl;
-    cout << evenOdd(23);
+    cout << mult(2,2) << endl;  //calling multiplication func
+    cout << evenOdd(23); //calling even-odd function
     return 0;
 }
